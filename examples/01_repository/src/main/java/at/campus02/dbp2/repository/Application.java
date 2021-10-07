6f027080b77ab4a1fba89098433159888f58be25
Caroline Chevillotte <contact@caroline-chevillotte.com>
2021-10-07 11:09:19 +0200

    InMemoryRepository

--- examples/01_repository/src/main/java/at/campus02/dbp2/repository/Application.java
+++ examples/01_repository/src/main/java/at/campus02/dbp2/repository/Application.java
@@ -3,10 +3,41 @@ package at.campus02.dbp2.repository;
 public class Application {
 
     public static void log(String msg) {
+
         System.out.println("Application:  --> " + msg);
     }
 
     public static void main(String[] args) {
+
         log("application started");
+
+        CustomerRepository repository = new InMemoryRepository();
+
+        Customer customer1 = new Customer();
+        customer1.setEmail("customer1@mail.com");
+        customer1.setFirstname("Carlo");
+        customer1.setLastname("Customer");
+
+        // 1) Create
+
+        repository.create(customer1);
+        log("Customer created: " + customer1);
+
+        // 2) Read
+        Customer fromRepository = repository.read(customer1.getEmail());
+        log("Customer read " + fromRepository);
+
+        // 3) Update
+        fromRepository.setFirstname("Conrad");
+        repository.update(fromRepository);
+        Customer updated = repository.read(fromRepository.getEmail());
+        log("Customer updated " + updated);
+
+
+        // 4) Delete
+        repository.delete(updated);
+        Customer deleted = repository.read(updated.getEmail());
+        log("Customer deleted: " + deleted);
+
     }
 }
---
+++ examples/01_repository/src/main/java/at/campus02/dbp2/repository/InMemoryRepository.java
@@ -0,0 +1,31 @@
+package at.campus02.dbp2.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InMemoryRepository implements CustomerRepository {
+
+    private final Map<String, Customer> storage = new HashMap<>();
+
+
+    @Override
+    public void create(Customer customer) {
+        // email is primary key
+        storage.put(customer.getEmail(), customer);
+    }
+
+    @Override
+    public Customer read(String email) {
+        return storage.get(email);
+    }
+
+    @Override
+    public void update(Customer customer) {
+        storage.replace(customer.getEmail(), customer);
+    }
+
+    @Override
+    public void delete(Customer customer) {
+        storage.remove(customer.getEmail(), customer);
+    }
+}
